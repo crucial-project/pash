4665edf2cf13509ed03a255ea76e1033a93ae048
Aurèle MAHEO <aurele.maheo@gmail.com>
2021-07-02 17:28:29 +0200

    sshell 1: update => bugy

--- scripts/sshell1.sh
+++ scripts/sshell1.sh
@@ -18,8 +18,44 @@ patternskip1="/pash/runtime/eager.sh"
 patternskip2="/pash/runtime/auto-split.sh"
 patternskip3="source"
 
-flagCmd=0
+flagCmd="false"
+flagPattern="false"
+flagMatch=false
 lastCmd=""
+
+# Check if given string matches any command
+matchCmd()
+{
+  	strl=$1
+	arrayCmd=$2
+  
+	IFS=', ' read -r -a arrayline <<< "$strl"
+
+        for index in "${!arrayline[@]}"
+	do
+		for indexCmd in ${!arrayCmd[@]}
+		do
+			if echo "${arrayline[$index]}" | grep -q "${arrayCmd[$indexCmd]}"
+			then
+				flagCmd="true"
+			fi
+		done
+	done
+}
+
+matchPattern()
+{
+
+	strl=$1 
+
+	if  echo "$line" | grep -wq "$pattern1"  ||  echo "$line" | grep -wq "$pattern2"  ||  echo "$line" | grep -wq "$pattern3"  ||  echo "$line" | grep -wq "$pattern4"  
+	then
+		echo PATTERN TRUE
+		flagPattern="true"
+	fi
+  
+}
+
 
 rm -f keyCmds.out
 touch keyCmds.out
@@ -27,25 +63,24 @@ touch keyCmds.out
 # 1st step: extract commands
 while read line 
 do
-
-	#if echo "$line" | grep -q "$patternskip1" || echo "$line" | grep -q "$patternskip2" || echo "$line" | grep -q "$patternskip3"
-	#then
-      	#	continue
-        #fi
+	if echo "$line" | grep -q "$patternskip1" || echo "$line" | grep -q "$patternskip2" || echo "$line" | grep -q "$patternskip3"
+	then
+      		continue
+        fi
 
 	#line=$(echo $line | sed 's/</< /g')
     	#line=$(echo $line | sed 's/>/> /g')
 	
 	if echo "$line" | grep -q "$pattern1" || echo "$line" | grep -q "$pattern2"
 	then
-		flagCmd=1
+		flagCmd=true
 	fi
 
 	IFS=', ' read -r -a arrayline <<< "$line"
        
         for index in "${!arrayline[@]}"
         do
-  		if [ $flagCmd == 1 ]
+  		if [ "$flagCmd" = true ]
 		then
 			if [[ $index == 0 && ${arrayline[0]} == "{" ]]
 			then
@@ -83,8 +118,11 @@ echo OUTPUT 1
 echo -e $output
 echo ==================================
 
+echo cat KeyCmds file :
 cat keyCmds.out
 
+echo ""
+echo ""
 arrayCmds=""
 nbStages=""
 
@@ -92,11 +130,15 @@ while read -r linecmd
 do
 	#itercmd=$((itercmd+1))
 	linecmd=$(echo $linecmd | sed "s/\"/'/g")
+
+	IFS=', ' read -r -a arraylinecmd <<< "$linecmd"
+
 	echo iter key: $itercmd
 
 	echo lineKey: $linecmd
 
 	arrayCmds[$itercmd]=$linecmd
+	arrayCmdFirst[$itercmd]=${arraylinecmd[0]}
 	#keyCmds[$iterKeys]=$linekey						
 done < keyCmds.out
 
@@ -138,7 +180,7 @@ do
 
 	if echo "${arrayline[1]}" | grep -q "${arrayCmds[nbStages-1]}"
 	then
-		echo KEY HIT	
+		echo ""	
 	fi
 
 done < routputtmp.out
@@ -146,18 +188,31 @@ done < routputtmp.out
 # prefix lines with sshell / ssh
 while read line
 do
+	flagCmd="false"
+	flagPattern="false"
+
 	IFS=', ' read -r -a arrayline <<< "$line"
 
-	if echo "$line" | grep -wq "$pattern1" || echo "$line" | grep -wq "$pattern2" || echo "$line" | grep -wq "$pattern3" || echo "$line" | grep -wq "$pattern4"
+	matchCmd $line ${arrayCmdFirst}
+	matchPattern $line
+
+	if  echo "$flagCmd" | grep -q "true"  ||  echo "$flagPattern" | grep -q "true"  
 	then
+		echo LINE TRUE
 		for index in "${!arrayline[@]}"
 		do
-			if echo "${arrayline[$index]}" | grep -wq  "$pattern1" || echo "${arrayline[$index]}" | grep -wq "$pattern2" || echo "${arrayline[$index]}" | grep -wq "$pattern3" || echo "${arrayline[$index]}" | grep -wq "$pattern4" 
+			flagCmd="false"
+			flagPattern="false"
+	
+			matchCmd ${arrayline[$index]} ${arrayCmdFirst}
+			matchPattern ${arrayline[$index]}
+
+			if echo "$flagCmd" | grep -q "true" || echo "$flagPattern" | grep -q "true" 
 			then
-				echo PATTERN HIT : ${arrayline[$index]}
+				#echo PATTERN HIT : ${arrayline[$index]}
 				output2="${output2} ${sshcmd} ${arrayline[$index]}" 
 			else
-				echo MISS
+				#echo MISS
 				output2="${output2} ${arrayline[$index]}" 
 			fi	
 		done
