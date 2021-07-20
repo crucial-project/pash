ca51f07bc73364d025db5b857c354d240faeeebd
Aurèle MAHEO <aurele.maheo@gmail.com>
2021-07-20 13:43:37 +0200

    sshell version 1: correct output + ssh command

--- scripts/sshell1.sh
+++ scripts/sshell1.sh
@@ -4,11 +4,11 @@ input=($@)
 
 csplit -z -f 'tempPASH' -b '%0d.txt' $input /mkfifo_pash_fifos/ {*}
 
-sed -i "s/{/{ ssh -t localhost '/g" tempPASH2.txt 
+sed -i "s/{ /{ ssh -tt localhost '/g" tempPASH2.txt 
 
 sed -i "s/&/' &/g" tempPASH2.txt 
 
-cat tempPASH0.txt > res.out 
-cat tempPASH1.txt >> res.out 
-cat tempPASH2.txt >> res.out 
+cat tempPASH0.txt > ssh_pash.sh
+cat tempPASH1.txt >> ssh_pash.sh
+cat tempPASH2.txt >> ssh_pash.sh
 
