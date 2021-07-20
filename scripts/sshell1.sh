770f6142a84a389de9ee6b211ecd162bb3f1c4e3
Aurèle MAHEO <aurele.maheo@gmail.com>
2021-07-20 14:53:38 +0200

    sshell version ssh: pick random machine among a list of given machines

--- scripts/sshell1.sh
+++ scripts/sshell1.sh
@@ -2,9 +2,27 @@
 
 input=($@)
 
+PAR=2
+
+arrssh=()
+while IFS= read -r line; do
+  arrssh+=("$line")
+done < configssh.txt
+
+echo ARRAY SSH MACHINES
+lenArraySSH=${#arrssh[@]}
+
+RANDOM=$$$(date +%s)
+
+sshmachine=${arrssh[$RANDOM % ${#arrssh[@]} ]}
+
+echo Random machine : $sshmachine
+
+echo $lenArraySSH
+
 csplit -z -f 'tempPASH' -b '%0d.txt' $input /mkfifo_pash_fifos/ {*}
 
-sed -i "s/{ /{ ssh -tt localhost '/g" tempPASH2.txt 
+sed -i "s/{ /{ ssh -tt $sshmachine '/g" tempPASH2.txt 
 
 sed -i "s/&/' &/g" tempPASH2.txt 
 
