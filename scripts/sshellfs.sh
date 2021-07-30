cc639fd749d183dc86eb4019386ee4ab506b10b8
MAHEO Aurele <amaheo@b313-01.int-evry.fr>
2021-07-30 17:34:52 +0200

    sshell version FS: update

--- scripts/sshellfs.sh
+++ scripts/sshellfs.sh
@@ -16,39 +16,65 @@ sendcmd="awk '{print \\\$0}END{print \\\"EOF\\\"}'"
 recvcmd1="tail -n +0 --pid=\\$\\$ --retry"
 recvcmd2="2>/dev/null | { sed '/EOF/ q' && kill \$\$ ;} | grep -v ^EOF\\$"
 
-output1="#!/usr/bin/env bash"
-output1="${output1} ${NEWLINE}"
-output1="${output1} ${NEWLINE}"
+output="#!/usr/bin/env bash"
+output="${output} ${NEWLINE}"
+output="${output} ${NEWLINE}"
 
 skippattern="/pash/runtime/eager.sh"
 
-while read line
-do
+#while read line
+#do
 	#if echo "$line" | grep -q "$skippattern"
 	#then
 	#	continue
 	#fi
 
-	output1="${output1} ${line}"
-	output1="${output1} ${NEWLINE}"
+	#output1="${output1} ${line}"
+	#output1="${output1} ${NEWLINE}"
 
-done < $input
+#done < $input 
 
-#echo $output1 > pipesshellfs_tmp.out
+#echo -e $output1 > pipesshellfs_tmp.out
+
+#sed -i "s/</< /g" pipesshellfs_tmp.out
+#sed -i "s/>/> /g" pipesshellfs_tmp.out
 
 #sed -i "s/<\"/touch/g" pipessshellfs_${PAR}_chunks.sh
 
-#while read line
-#do
-	#if echo "$line" | grep -q "<"
-	#then
-			
-	#fi	
+while read line
+do
+	line=$(echo $line | sed 's/\<mkfifo\>/touch/g')
+	line=$(echo $line | sed 's/</< /g')
+	line=$(echo $line | sed 's/>/> /g')
 
-#done < $output1
+	if echo "$line" | grep -q "<"
+	then
+		IFS='<' read -r cmd subline <<< "$line"
+		cmd=$(echo $cmd | sed 's/{/ /g')
+ 		echo command: $cmd
+		echo line1: $subline
+		#cmd=$arrayline[1]
+		#file1=$arrayline[3]
+		#file2=$arrayline[5]
+
+		if echo "$subline" | grep -q ">"
+		then
+			IFS='>' read -r file1 file2 <<< "$subline"
+			file2=$(echo $file2 | sed 's/& }//g')
+			echo file1: $file1
+			echo file2: $file2
+
+		fi
+		output="${output} { ${recvcmd1} ${file1} ${recvcmd2} | ${cmd} | ${sendcmd} > ${file2} & }"			
+	else	
+		output="${output} ${line}"
+	fi
+
+	output="${output} ${NEWLINE}"
+
+done < $input
 
 echo OUTPUT
 echo ==================
-echo -e $output1 > pipessshellfs_${PAR}_chunks.sh 
+echo -e $output > pipessshellfs_chunks.sh 
 
-sed -i "s/\<mkfifo\>/touch/g" pipessshellfs_${PAR}_chunks.sh
